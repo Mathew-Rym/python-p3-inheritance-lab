4a73f66b86c32d26160a0c4bd9e19d1f06634cce
Mathew-Rym <rymmathew@gmail.com>
2025-05-19 00:48:03 +0300

    passed

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,15 @@
-#!/usr/bin/env python
-
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []  # Initialize knowledge as an empty list
     
-    def learn(self):
-        pass
+    def learn(self, skill):
+        self.knowledge.append(skill)
+        return self.knowledge
+
+# Sample Student instance to test
+student1 = Student("juma", "Isaiah")
+print(student1.first_name, student1.last_name)
+print(student1.learn("JavaScript"))
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,27 @@
-#!/usr/bin/env python
-
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # Make sure we call the parent constructor
+        # Initialize knowledge as a list of strings (non-empty)
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
-        pass
+        # Return a random knowledge from the list
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
+
+
+# Sample instance to verify
+teacher1 = Teacher("Julius", "Mwangi")
+print(teacher1.first_name, teacher1.last_name)  # Prints the teacher's name
+print(teacher1.teach())  # Should print a random knowledge string from the list
